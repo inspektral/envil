5b8cfbe16496a6cf0a3e88951d78f4eb51af64da
killbizz <gabriel.bizzo@hotmail.it>
2024-05-31 23:36:08 +0200

    added settings.json, snippets, updated extension logic keeping only one hydra evaluation command

--- extension.js
+++ extension.js
@@ -18,9 +18,11 @@ function activate(context) {
 	let io;
 
 	let helloWorld = vscode.commands.registerCommand('envil.helloWorld', function () {
-		// vscode.window.showInformationMessage('Hello World from envil!');
-		
+
 		app.use(express.static(path.join(__dirname, 'hydrascripts')));
+
+		// Serve static files from the 'public' directory
+		app.use('/images', express.static(path.join(__dirname, 'public')));
 
 		app.listen(3000, () => {
 			console.log('Server is running at http://localhost:3000');
@@ -34,41 +36,47 @@ function activate(context) {
 		});
 
 		io.on('connection', (socket) => {
-		console.log('terminal server : Client connected');
+		console.log('socket : Client connected');
 		socket.on('disconnect', () => {
-			console.log('terminal server : Client disconnected');
+			console.log('socket : Client disconnected');
 		});
 		});
 	});
 
-	let evaluate = vscode.commands.registerCommand('envil.evaluate', function () {
+	let evaluate = vscode.commands.registerCommand('envil.evaluate.hydra', function () {
 		const editor = vscode.window.activeTextEditor;
 		if (editor) {
+			let command = "";
 			const document = editor.document;
 			const selection = editor.selection;
 			let text = document.getText(selection);
-			if (text.length === 0) {
-				let lineNumber = selection.active.line;
-				text = editor.document.lineAt(lineNumber).text.trim();
+			text = text.length === 0 ? document.getText() : text;
+			const lines = text.split('\n');
+			for (let line of lines) {
+				if(!line.includes('://') && line.includes('//')) {
+					line = "";
+				}
+				// local images handling
+				if(line.includes('local/images/')) {
+					line = line.replace("local/images/", "http://localhost:3000/images/");
+				}
+				if(line == "cls") {
+					console.clear();
+				} else if(line != ""){
+					command = command + line;
+					if(line.endsWith(';')) {
+						// send command to client
+						console.log("\n\n");
+						io.sockets.emit('new-command', { data: command});
+						command = "";
+					}
+				}
 			}
-			console.log(text);
-			io.emit('new-command', {data:text});
-		}
-	});
-
-	let evaluateAll = vscode.commands.registerCommand('envil.evaluateAll', function () {
-		const editor = vscode.window.activeTextEditor;
-		if (editor) {
-			const document = editor.document;
-			let text = document.getText();
-			console.log(text);
-			io.emit('new-command', {data:text});
 		}
 	});
 
 	context.subscriptions.push(helloWorld);
 	context.subscriptions.push(evaluate);
-	context.subscriptions.push(evaluateAll);
 }
 
 // This method is called when your extension is deactivated
