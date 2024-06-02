7178b488393c4e5728d26723914b165b1addb92e
killbizz <gabriel.bizzo@hotmail.it>
2024-06-03 00:56:27 +0200

    fixed adding/removing settings mechanism, to test APC extension activation/deactivation

--- extension.js
+++ extension.js
@@ -1,3 +1,4 @@
+// @ts-nocheck
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 const vscode = require('vscode');
@@ -6,6 +7,7 @@ const { Server } = require('socket.io');
 const path = require('path');
 const fs = require('fs');
 const os = require('os');
+const jsonc = require('jsonc-parser');
 
 let app;
 let server;
@@ -14,62 +16,74 @@ let io;
 /**
  * @param {vscode.ExtensionContext} context
  */
-function activate(context) {
+async function activate(context) {
     console.log('Activating ENVIL Extension');
+
+    await vscode.commands.executeCommand('apc.extension.enable');
 
     let openEnvironmentCommand = vscode.commands.registerCommand('envil.start', async function () {
 		try {
+            await vscode.commands.executeCommand('apc.extension.enable');
+            let restartNeeded = false;
             // Update workspace settings
             const workspaceFolder = vscode.workspace.workspaceFolders ? vscode.workspace.workspaceFolders[0].uri.fsPath : null;
             if (workspaceFolder) {
                 const workspaceSettingsPath = path.join(workspaceFolder, '.vscode', 'settings.json');
                 await createSettingsFileIfNotExist(workspaceSettingsPath);
                 const newWorkspaceSettingsPath = path.join(__dirname, 'data', 'workspace_settings.json');
-                const newWorkspaceSettings = JSON.parse(fs.readFileSync(newWorkspaceSettingsPath, 'utf8'));
-                updateSettingsWithPlaceholders(workspaceSettingsPath, newWorkspaceSettings);
+                const newWorkspaceSettings = readJsonWithComments(newWorkspaceSettingsPath).json;
+                if (fs.existsSync(workspaceSettingsPath) && 
+                    areEnvironmentSettingsNotPresent(fs.readFileSync(workspaceSettingsPath, 'utf-8'))) {
+                        addSettingsWithPlaceholders(workspaceSettingsPath, newWorkspaceSettings);
+                        restartNeeded = true;
+                }
             }
-
-            // TODO : test
             // Update user settings
             const globalSettingsPath = getGlobalSettingsPath();
             const newGlobalSettingsPath = path.join(__dirname, 'data', 'global_settings.json');
-            const newGlobalSettings = JSON.parse(fs.readFileSync(newGlobalSettingsPath, 'utf8'));
-            updateSettingsWithPlaceholders(globalSettingsPath, newGlobalSettings);
-
-            vscode.window.showInformationMessage('Configuration settings updated!');
-        } catch (error) {
-            vscode.window.showErrorMessage(`Failed to update settings: ${error.message}`);
-        }
-
-        if (app || server || io) {
-            closeServersAndSockets();
-        }
-
-        // create servers
-        app = express();
-        server = app.listen(3000, () => {
-            console.log('Express server is running at http://localhost:3000');
-            vscode.env.openExternal(vscode.Uri.parse('http://localhost:3000'));
-        });
-        io = new Server(3001, {
-            cors: {
-                origin: '*',
+            const newGlobalSettings = readJsonWithComments(newGlobalSettingsPath).json;
+            if (fs.existsSync(globalSettingsPath) && 
+                areEnvironmentSettingsNotPresent(fs.readFileSync(globalSettingsPath, 'utf-8'))) {
+                    addSettingsWithPlaceholders(globalSettingsPath, newGlobalSettings);
+                    restartNeeded = true;
             }
-        });
-        io.on('connection', (socket) => {
-            console.log('Socket.io: Client connected');
-            socket.on('disconnect', () => {
-                console.log('Socket.io: Client disconnected');
-            });
-        });
+            if(restartNeeded){
+                promptRestart();
+            }
 
-        // serve static files
-        app.use(express.static(path.join(__dirname, 'hydra')));
-        const workspaceFolder = vscode.workspace.workspaceFolders ? vscode.workspace.workspaceFolders[0].uri.fsPath : null;
-        if (workspaceFolder) {
-            app.use('/files', express.static(path.join(workspaceFolder, 'public')));
-        } else {
-            console.log("Can't serve static local files: No workspace folder is open.");
+            // shut down servers if needed
+            if (app || server || io) {
+                closeServersAndSockets();
+            }
+
+            // create servers
+            app = express();
+            server = app.listen(3000, () => {
+                console.log('Express server is running at http://localhost:3000');
+                vscode.env.openExternal(vscode.Uri.parse('http://localhost:3000'));
+            });
+            io = new Server(3001, {
+                cors: {
+                    origin: '*',
+                }
+            });
+            io.on('connection', (socket) => {
+                console.log('Socket.io: Client connected');
+                socket.on('disconnect', () => {
+                    console.log('Socket.io: Client disconnected');
+                });
+            });
+
+            // serve static files
+            app.use(express.static(path.join(__dirname, 'hydra')));
+            if (workspaceFolder) {
+                app.use('/files', express.static(path.join(workspaceFolder, 'public')));
+            } else {
+                vscode.window.showErrorMessage("Can't serve static local files: No workspace folder is open.");
+            }
+                
+        } catch (error) {
+            vscode.window.showErrorMessage(`Failed to load environment: ${error.message}`);
         }
     });
 
@@ -81,17 +95,14 @@ function activate(context) {
                 const workspaceSettingsPath = path.join(workspaceFolder, '.vscode', 'settings.json');
                 removeSettingsWithPlaceholders(workspaceSettingsPath);
             }
-
             // remove user settings
             const globalSettingsPath = getGlobalSettingsPath();
             removeSettingsWithPlaceholders(globalSettingsPath);
-
-            vscode.window.showInformationMessage('Configuration settings removed!');
+            closeServersAndSockets();
+            promptRestart();
         } catch (error) {
-            vscode.window.showErrorMessage(`Failed to remove settings: ${error.message}`);
+            vscode.window.showErrorMessage(`Failed to close the environment: ${error.message}`);
         }
-        
-        closeServersAndSockets();
     });
 
     let evaluateHydraCommand = vscode.commands.registerCommand('envil.evaluate.hydra', function () {
@@ -175,72 +186,99 @@ function getGlobalSettingsPath() {
     return path.join(homeDir, vscodeSettingsFolder, 'settings.json');
 }
 
-async function updateSettingsWithPlaceholders(settingsPath, newSettings) {
+async function addSettingsWithPlaceholders(settingsPath, newSettings) {
     try {
-        let existingSettings = {};
-        let settingsContent = '';
-        const newSettingsString = JSON.stringify(newSettings, null, "\t");
-
-        // Check if the settings file exists
-        if (fs.existsSync(settingsPath)) {
-            // Read existing settings from the file
-            settingsContent = fs.readFileSync(settingsPath, 'utf-8');
-            existingSettings = extractSettingsFromPlaceholders(settingsContent);
-        }
-
-        const wrappedNewSettings = wrapSettingsWithPlaceholders(newSettingsString.substring(1, newSettingsString.length - 1));
-
-        const existingSettingsContent = isEmpty(existingSettings) ? "{ " : JSON.stringify(existingSettings, null, "\t");
-
-        // Merge existing settings with new settings
-        const mergedSettings = existingSettingsContent.substring(0, existingSettingsContent.length - 1)
-            .concat(isEmpty(existingSettings) ? '' : ',')
-            .concat(wrappedNewSettings + '}');
-
-        // Write updated settings to the file
-        fs.writeFileSync(settingsPath, mergedSettings);
-
-        vscode.window.showInformationMessage('Configuration settings updated!');
+        // Read existing settings from the file
+        updateJsonWithComments(settingsPath, newSettings);
     } catch (err) {
+        console.error(err);
         vscode.window.showErrorMessage(`Failed to update settings file: ${err.message}`);
+        throw err;
     }
-}
-
-function extractSettingsFromPlaceholders(settingsContent) {
-    const regex = /\/\/ BEGIN: ENVIL Extension Settings[\s\S]*?\/\/ END: ENVIL Extension Settings/;
-    const parts = settingsContent.split(regex);
-    const outsidePlaceholders = removeLastComma(parts[0]) + parts[1];
-    return JSON.parse(outsidePlaceholders);
-}
-
-function wrapSettingsWithPlaceholders(settingsContent) {
-    // Replace settings between placeholders with new settings
-    // return settingsContent.replace(/\/\/ BEGIN: ENVIL Extension Settings\n[\s\S]*?\/\/ END: ENVIL Extension Settings\n/, `// BEGIN: ENVIL Extension Settings\n${newSettingsContent}\n// END: ENVIL Extension Settings\n`);
-    const settingsContentString = settingsContent.substring(1, settingsContent.length - 1);
-    return `\n\t// BEGIN: ENVIL Extension Settings\n ${settingsContentString} \n\t// END: ENVIL Extension Settings\n`;
-}
-
-function isEmpty(obj) {
-    return JSON.stringify(obj) === '{}' && obj.constructor === Object;
-}
-
-function removeLastComma(str) {
-    const lastCommaIndex = str.lastIndexOf(',');
-    if (lastCommaIndex === -1) {
-        return str;
-    }
-    return str.slice(0, lastCommaIndex) + str.slice(lastCommaIndex + 1);
 }
 
 async function removeSettingsWithPlaceholders(settingsPath) {
     try {
         const settingsContent = fs.readFileSync(settingsPath, 'utf-8');
+
         // Remove custom settings between placeholders
         const updatedContent = settingsContent.replace(/\/\/ BEGIN: ENVIL Extension Settings[\s\S]*?\/\/ END: ENVIL Extension Settings/, '');
+        
+        // Remove the comma after the last property inside the root of the JSON content
+        const cleanedContent = updatedContent.replace(/,(?=\s*[\r\n]*\})/, '');
+        // Remove the empty line after the last property
+        const finalContent = cleanedContent.replace(/\n\s*\n\s*(?=\})/, '\n');
+
         // Write updated settings
-        fs.writeFileSync(settingsPath, updatedContent.trim());
+        fs.writeFileSync(settingsPath, finalContent.trim());
     } catch (err) {
         vscode.window.showErrorMessage(`Failed to remove settings file: ${err.message}`);
+    }
+}
+
+function areEnvironmentSettingsNotPresent(settingsContent) {
+    const regex = /\/\/ BEGIN: ENVIL Extension Settings[\s\S]*?\/\/ END: ENVIL Extension Settings/;
+    return !regex.test(settingsContent);
+}
+
+function readJsonWithComments(filePath) {
+    const content = fs.readFileSync(filePath, 'utf-8');
+    const errors = [];
+    const json = jsonc.parse(content, errors);
+    if (errors.length) {
+        console.error('Error parsing JSON:', errors);
+        return null;
+    }
+    return { json, content };
+}
+
+function addPlaceholdersAndSettings(content, updates, startComment, endComment) {
+    // Check if the last character before the closing bracket is a comma
+    const needsComma = content.match(/"[^"]+"\s*:\s*"[^"]+"(?:\s*,\s*)?(\n*\s*\})/);
+
+    // Insert start comment before the closing bracket
+    let updatedContent = content.replace(/(\s*\})$/, `${needsComma ? ',' : ''}\n\t${startComment}\n$1`);
+
+    // Add each key-value pair from updates
+    const updateEntries = Object.entries(updates);
+    updateEntries.forEach(([key, value], index) => {
+        // Add a comma if not the last entry in updates
+        const comma = index === updateEntries.length - 1 ? '' : ',';
+        // Convert key-value pair to string
+        const entryString = `    "${key}": ${JSON.stringify(value, null, 4).replace(/\n/g, '\n    ')}${comma}`;
+        // Insert the entry before the closing bracket
+        updatedContent = updatedContent.replace(/(\n*\s*\})$/, `\n${entryString}$1`);
+    });
+
+    // Insert end comment after the last update and before the closing bracket
+    updatedContent = updatedContent.replace(/(\n*\s*\})$/, `\n\t${endComment}\n$1`);
+
+    // Remove empty lines between the last comment and the closing bracket
+    updatedContent = updatedContent.replace(/\n*(\s*\})$/, '\n$1');
+
+    return updatedContent;
+}
+
+function updateJsonWithComments(filePath, updates) {
+    const startComment = "// BEGIN: ENVIL Extension Settings";
+    const endComment = "// END: ENVIL Extension Settings"
+    let { json, content } = readJsonWithComments(filePath);
+    if (json === null || content === '') {
+        content = '{}';
+    }
+    const updatedContent = addPlaceholdersAndSettings(content, updates, startComment, endComment);
+    fs.writeFileSync(filePath, updatedContent, 'utf-8');
+}
+
+async function promptRestart() {
+    const restartAction = 'Restart';
+    const response = await vscode.window.showWarningMessage(
+        'Changes have been made that require a restart to take effect. Would you like to restart now?',
+        restartAction
+    );
+
+    if (response === restartAction) {
+        vscode.commands.executeCommand('workbench.action.reloadWindow');
     }
 }
 
